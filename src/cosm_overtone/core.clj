aeb6aac026575fab4869f2f751780914595a4298
Levent Ali <lebreeze@gmail.com>
2012-10-28 07:46:29 +0000

    Hello world -main

--- src/cosm_overtone/core.clj
+++ src/cosm_overtone/core.clj
@@ -1,6 +1,12 @@
-(ns cosm-overtone.core)
+(ns cosm-overtone.core
+  (:require [clj-http.client :as client]))
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(defn -main
+  "Called by lein run"
+  [& args]
+  (foo "Overtone"))
